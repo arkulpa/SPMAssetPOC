1b25697c16b64485074ebcca710400df12be4af1
Stefan Walkner <stefan@arkulpa.at>
2022-01-26 10:35:33 +0100

    Initial Commit

---
+++ SPMAssetPoc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SPMAssetPoc
+//
+//  Created by Stefan Walkner on 26.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SPMAssetPoc/SPMAssetPocApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMAssetPocApp.swift
+//  SPMAssetPoc
+//
+//  Created by Stefan Walkner on 26.01.22.
+//
+
+import SwiftUI
+
+@main
+struct SPMAssetPocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
